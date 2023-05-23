4ce4a3196b05747f85f77ae3e997353df99a6de5
Hakkene <dogier2006@gmail.com>
2023-05-23 05:32:50 +0200

    work in progress searchscreen animalscreen

--- lib/screens/searchscreen.dart
+++ lib/screens/searchscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:zooapp/widgets/sql_helper.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -8,13 +9,18 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  late Future<List<Map<String, dynamic>>> _animalsFuture;
+  late Future<List<Map<String, dynamic>>> animalsList;
 
   @override
   void initState() {
     super.initState();
-    _animalsFuture = SQLHelper.getAnimals();
+    // fetchAnimals();
+    animalsList = SQLHelper.getAnimalsList();
   }
+
+  // Future<void> fetchAnimals() async {
+  //   animalsList = await SQLHelper.getAnimals();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -22,42 +28,96 @@ class _SearchScreenState extends State<SearchScreen> {
       appBar: AppBar(
         title: const Text('Lista zwierzaków'),
       ),
-      body: FutureBuilder<List<Map<String, dynamic>>>(
-        future: _animalsFuture,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final animals = snapshot.data!.map((map) => Animal(
-                  id: map['id'],
-                  name: map['name'],
-                  description: map['description'],
-                  visited: map['visited'],
-                  onlist: map['onlist'],
-                  photographed: map['photographed'],
-                ));
-            return ListView.builder(
-              itemCount: animals.length,
-              itemBuilder: (context, index) {
-                final animal = animals.elementAt(index);
-                return ListTile(
-                  title: Text(animal.name),
-                  subtitle: Text(animal.description),
-                  leading: Icon(Icons.pets),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                  ),
-                );
+      body: Column(
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+            child: InkWell(
+              onTap: () {
+                print("tapped");
               },
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('Error loading data.'),
-            );
-          } else {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+              child: Container(
+                alignment: Alignment.center,
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: "Nazwa zwierzaka",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(28.0),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(vertical: 12.0),
+                    suffixIcon: const Icon(Icons.search),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: FutureBuilder<List<Map<String, dynamic>>>(
+                future: animalsList,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.separated(
+                        separatorBuilder: (context, index) =>
+                            const Divider(), // Add Divider widget
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            leading: ConstrainedBox(
+                              constraints: BoxConstraints(
+                                minWidth: 60,
+                                maxWidth: 60,
+                                minHeight: 60,
+                                maxHeight: 60,
+                              ),
+                              child: Image.asset(
+                                "lib/assets/images/catSquare.png",
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            title: Text(
+                              snapshot.data![index]["name"],
+                            ),
+                            subtitle: Text(
+                              snapshot.data![index]["description"],
+                            ),
+                            onTap: () {
+                              context.push("/animal");
+                            },
+                          );
+                        });
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              ),
+            ),
+          )
+          // Expanded(
+          //    child: ListView.builder(
+          //     itemCount: 10,
+          //     itemBuilder: (context, index) {
+          //       return GestureDetector(
+          //         onTap: () {
+          //           print("tapped");
+          //         },
+          //         child: Padding(
+          //           padding: const EdgeInsets.all(8.0),
+          //           child: Container(
+          //             color: Colors.grey,
+          //             height: 100.0,
+          //           ),
+          //         ),
+          //       );
+          //     },
+          //   ),
+          // ),
+        ],
       ),
     );
   }
